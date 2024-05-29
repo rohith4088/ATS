96def3b01a1245ce6799f8440efc5aba975be8c6
rohith4088 <2109486@reva.edu.in>
2024-05-29 18:20:05 +0530

    minor changes

--- app.py
+++ app.py
@@ -26,10 +26,10 @@ def input_pdf_text(uploaded_file):
 
 input_prompt="""
 Hey Act Like a skilled or very experience ATS(Application Tracking System)
-with a deep understanding of tech field,software engineering,data science ,data analyst
+with a deep understanding of tech field,software engineering,data science ,data analyst,machine learning engineer
 and big data engineer. Your task is to evaluate the resume based on the given job description.
 You must consider the job market is very competitive and you should provide 
-best assistance for improving thr resumes. Assign the percentage Matching based 
+best assistance for improving thr resumes.Assign the percentage Matching based 
 on Jd and
 the missing keywords with high accuracy
 resume:{text}
